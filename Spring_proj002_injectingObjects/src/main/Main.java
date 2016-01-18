0ea9caa0fe222b7405f12492d8218f186623dcbd
atafs <atafs@iscte-iul.pt>
2016-01-18 18:02:49 +0000

    update

--- Spring_proj002_injectingObjects/src/main/Main.java
+++ Spring_proj002_injectingObjects/src/main/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		//INSTANTIATE
+		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("config/spring.xml");
 		Triangle triangle = (Triangle)context.getBean("triangle-alias");
 		triangle.draw();
